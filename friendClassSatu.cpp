07ab2c413b7cc8a34653ace7c316b8b9b4352482
faiqzzz <ridhofaiqzzz@gmail.com>
2025-05-05 19:52:26 +0700

    menampilkan  set nama

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -11,3 +11,15 @@ public:
     friend void setNama (mahasiswa &a, string);
 };
 
+void setnama (mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setnama (joko, "kairi Kumar");
+    return 0;
+}
