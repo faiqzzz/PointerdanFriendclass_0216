7d57da4c3df35c7285552cf2ca13abdb3841e203
faiqzzz <ridhofaiqzzz@gmail.com>
2025-05-05 19:36:28 +0700

    menampilkan id dan nama

--- PointerClassSatu.cpp
+++ PointerClassSatu.cpp
@@ -32,3 +32,17 @@ void siswa:: setId(int pId)
     id = pId;
 }
 
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl;
+    cout << a.nama;   
+}
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll (joko);
+    return 0;
+}
