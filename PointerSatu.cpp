a1cb7d4e593736cb30b9ede351f5fccc33e46003
faiqzzz <ridhofaiqzzz@gmail.com>
2025-05-05 19:03:19 +0700

    pointer & object

--- PointerSatu.cpp
+++ PointerSatu.cpp
@@ -12,3 +12,18 @@ class mahasiswa
 
 };
 
+int main()
+{
+    mahasiswa mhs; // Object mhs
+    mhs.nim = 2024;
+    mhs.showNim(); // member Access Operator
+
+    mahasiswa &refMhs = mhs; // Pointer reference refMhs
+    refMhs.nim = 2;          // Member Access Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;  // Pointer Deference pHms
+    pMhs-> nim = 3;
+    pMhs-> showNim();
+    return 0;
+}
